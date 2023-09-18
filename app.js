24b03c458528f4fc641f0d259b4c73c14d708447
Mitul Sonagara <mitulkumarsongara@gmail.com>
2023-09-19 00:04:52 +0530

    session and logout

--- app.js
+++ app.js
@@ -3,17 +3,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-// const md5 = require('md5');
-const bcrypt = require('bcrypt');
-
-const saltRound = 10
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+
+app.use(session({
+    secret: "This is my secret key",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -30,9 +37,14 @@ const userSchema = new mongoose.Schema({
     password: String,
 })
 
-// userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]})
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', (req, res) => {
     res.render("home")
@@ -46,42 +58,55 @@ app.get('/register', (req, res) => {
     res.render("register")
 });
 
+app.get('/secrets', (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render("secrets")
+    } else {
+        res.redirect("/login")
+    }
+});
+
+app.get('/logout', (req, res) => {
+    req.logout((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/")
+        }
+    });
+})
+
 app.post('/register', async (req, res) => {
 
-    await bcrypt.hash(req.body.password, saltRound, async (err, hash) => {
-        await User.create({
-            email: req.body.username,
-            password: hash
-        })
-
-        try {
-            res.render("secrets")
-        } catch (err) {
-            console.error(err);
-            res.render("register", { errorMessage: "Registration failed. Please try again." });
+    User.register({ username: req.body.username }, req.body.password, (err, user) => {
+        if (err) {
+            console.log(err)
+            res.redirect("/register");
+        } else {
+            passport.authenticate('local')(req, res, () => {
+                res.redirect('/secrets')
+            })
         }
-
     })
+
 });
 
 app.post('/login', async (req, res) => {
-    const username = req.body.username
-    const entered_password = req.body.password
 
-    try {
-        const userfound = await User.findOne({ email: username })
-        bcrypt.compare(entered_password, userfound.password, (err, result) => {
-            if (result = true) {
-                res.render("secrets")
-            } else {
-                res.render("login")
-            }
-        })
-    } catch (err) {
-        console.error(err);
-        res.render("login", { errorMessage: "Invalid email or password. Please try again." })
-    }
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
 
+    req.login(user, (err) => {
+        if (err) {
+            console.log(err)
+        } else {
+            passport.authenticate('local')(req, res, () => {
+                res.redirect('/secrets')
+            });
+        }
+    })
 
 })
 
